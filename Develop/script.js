2d3bbb50e0555eb5fe5210e1908ff6ba23d4a7b5
TrangTheDev <trangh1@hotmail.com>
2021-09-04 10:25:43 +1000

    First commit

--- Develop/script.js
+++ Develop/script.js
@@ -12,3 +12,124 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+/ Array of special characters to be included in password
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.',
+];
+
+// Array of numeric characters to be included in password
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+// Array of lowercase characters to be included in password
+var lowerCasedCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+];
+// Array of uppercase characters to be included in password
+var upperCasedCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+];
+//choose the character function//
+
+var Character = function() {
+  // Ask user for the number of characters
+  var userChoice = prompt("How many number of characters in the passcode");
+  
+
+  //choose the uppercase function//
+
+  var uppercase = function() {
+    // Ask user for the number of characters
+    var userchooseuppercase = prompt("Do you want to include uppercase in your passcode");
+  
+
+  //choose the lowerlast funtion//
+
+  var lowercase = function() {
+    // Ask user for the number of characters
+    var userchooseloswercase = prompt("Do you want to include lowercase in your passcode");
+  
+
+
+  //choose the symbol funtion//
+  var special = function() {
+    // Ask user for the number of characters
+    var userchoosespecial = prompt("Do you want to include special characters in your passcode");
+  
+
+  // Get random index from array of options
+  var index = Math.floor(Math.random() * options.length);
+  var generatePassword = options[index];
